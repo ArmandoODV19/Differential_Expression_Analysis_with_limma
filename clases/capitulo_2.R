d7201b57ec5c6c018242d0e28307ef9692a8eb1a
armandios19 <fooseaov@gmail.com>
2022-07-19 15:55:01 -0500

    clase

--- clases/capitulo_2.R
+++ clases/capitulo_2.R
@@ -33,3 +33,31 @@ fit2 <- eBayes(fit2)
 # se cuenta el numero de genes expresados diferencialmente
 results <- decideTests(fit2)
 summary(results)
+
+
+### modelando estudios con mas de dos grupos
+
+BiocManager::install("leukemiasEset")
+library(leukemiasEset)
+
+# para este jerccio se utilizara el data set de leukemiaeset
+# ya que contiene tres grupos ALL, AML, CML
+
+data("leukemiasEset")
+
+dim(leukemiasEset)
+
+# se coloca el nombre de la columna de leukemiasEset en phenoData
+# para obtner el tipo de leucemias que se han descrito en el dataset
+table(pData(leukemiasEset)[,"LeukemiaType"])
+
+# para examinar cada grupo se realiza una formula que
+# incluye 0 para remover la intercepcion y el nombre de la columna
+# que contiene el nombre de las muestras
+
+design <- model.matrix(~0 + LeukemiaType,
+                       data = pData(leukemiasEset))
+
+head(design, 3)
+
+colSums(design)
