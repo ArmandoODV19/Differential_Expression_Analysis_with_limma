37e6229aef73d02c7a7916f3f1e3765b93b3a9a1
armandios19 <fooseaov@gmail.com>
2022-07-20 17:46:45 -0500

    clase

--- clases/capitulo_4.R
+++ clases/capitulo_4.R
@@ -8,3 +8,9 @@ plotDensities(dox,
               group = pData(dox)[, "genotype"],
               legend = "topright")
 
+x <- dox@assayData[["exprs"]]
+p <- dox@phenoData@data
+f <- dox@featureData@data
+
+boxplot(x["A_51_P227777",]~p[,"treatment"],
+        main = f["A_51_P227777", "symbol"])
