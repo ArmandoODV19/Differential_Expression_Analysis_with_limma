2fc84f2aee2d5d3c91a421005d66d50eed644300
armandios19 <fooseaov@gmail.com>
2022-07-20 17:38:43 -0500

    clase

--- clases/capitulo_4.R
+++ clases/capitulo_4.R
@@ -2,3 +2,9 @@
 dim(dox)
 
 table(pData(dox)[,c("genotype", "treatment")])
+
+# procesando datos
+plotDensities(dox,
+              group = pData(dox)[, "genotype"],
+              legend = "topright")
+
